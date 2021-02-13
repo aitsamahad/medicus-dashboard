599619ebcc8b10bbd06d858e80508bfc016fbb2a
aitsamahad <aitsama.ahad@gmail.com>
2021-02-13 18:40:53 +0500

    removed instant.page script

--- pages/_document.js
+++ pages/_document.js
@@ -18,11 +18,6 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script
-            src="//instant.page/5.1.0"
-            type="module"
-            integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"
-          ></script>
         </body>
       </Html>
     );
