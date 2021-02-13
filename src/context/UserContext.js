864ab3534a1cb4b90fbe0805118e87265b51e883
aitsamahad <aitsama.ahad@gmail.com>
2021-02-13 17:42:27 +0500

    Added Context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,9 +1,28 @@
 import React, { createContext } from "react";
+import { getAllDoctors } from "../controllers/doctor";
 
 export const UserContext = createContext();
 
 function UserContextProvider({ children }) {
-  const contextValues = {};
+  const [doctors, setDoctors] = React.useState();
+  const [token, setToken] = React.useState(
+    typeof window !== "undefined" && window.localStorage.getItem("token")
+  );
+
+  React.useEffect(() => {
+    getDoctors();
+  }, []);
+
+  const getDoctors = () => {
+    return getAllDoctors().then((res) => setDoctors(res));
+  };
+
+  const contextValues = {
+    getDoctors,
+    doctors,
+    token,
+    setToken,
+  };
 
   return (
     <UserContext.Provider
