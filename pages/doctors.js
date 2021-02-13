8d160a2ec0a79437b50640381005254252b8e77b
aitsamahad <aitsama.ahad@gmail.com>
2021-02-13 17:41:44 +0500

    Added condition for when admin is not logged in

--- pages/doctors.js
+++ pages/doctors.js
@@ -3,21 +3,29 @@ import DoctorsPage from "../src/components/DoctorsPage";
 import { apiUrl, swrfetcher } from "../src/utils/fetcher";
 import useSWR from "swr";
 import { getAllDoctors } from "../src/controllers/doctor";
+import { UserContext } from "../src/context/UserContext";
+import SignIn from "../src/components/Signin";
 
-export async function getServerSideProps() {
-  const doctors = await getAllDoctors();
+// export async function getServerSideProps() {
+//   const doctors = await getAllDoctors();
 
-  return {
-    props: {
-      doctors,
-    },
-  };
-}
+//   return {
+//     props: {
+//       doctors,
+//     },
+//   };
+// }
 
-export default function doctors({ doctors }) {
+export default function doctors() {
+  const { doctors, token, setToken } = React.useContext(UserContext);
+  console.log("DOCTORS LIST: ", doctors);
   return (
-    <>
-      <DoctorsPage doctors={doctors} />
-    </>
+    <div style={{ minHeight: 800 }}>
+      {token ? (
+        <>{doctors && <DoctorsPage doctors={doctors} />}</>
+      ) : (
+        <SignIn setToken={setToken} />
+      )}
+    </div>
   );
 }
